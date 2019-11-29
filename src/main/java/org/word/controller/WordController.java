e7c54c3ef292a11503d2ef9ccb230db890f708d2
caibingfang <caibingfang@bubi.cn>
2019-11-29 14:49:33 +0800

    fix:添加输入页面

--- src/main/java/org/word/controller/WordController.java
+++ src/main/java/org/word/controller/WordController.java
@@ -1,13 +1,20 @@
 package org.word.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.word.dto.Table;
 import org.word.service.WordService;
 
+import javax.servlet.http.HttpServletRequest;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.List;
 import java.util.Map;
 
@@ -19,6 +26,9 @@ public class WordController {
 
     @Autowired
     private WordService tableService;
+
+    @Autowired
+    private HttpServletRequest request;
 
     /**
      * @param model
@@ -32,4 +42,29 @@ public class WordController {
         model.addAttribute("maps", tables);
         return "word";
     }
+
+    @RequestMapping("/export")
+    public ResponseEntity<byte[]> export(@RequestParam(value = "url", required = true) String url) throws Exception {
+        String target = request.getScheme() +"://" + request.getServerName()
+                + ":" +request.getServerPort()
+                + "/toWord?url="+url.trim();
+        URL targetUrl = new URL(target);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        headers.add("Content-Disposition", "attachment; filename=swagger.doc;filename*=utf-8''"+ URLEncoder.encode("swagger.doc","UTF-8"));
+        headers.add("Pragma", "no-cache");
+        headers.add("Expires", "0");
+
+        return ResponseEntity
+                .ok()
+                .headers(headers)
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(FileCopyUtils.copyToByteArray(targetUrl.openStream()));
+
+
+    }
+
+
+
 }
